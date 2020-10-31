cb21bc8ef1b5d5a5ce37a51aea663842d79d114a
zpdelong <zacherypd@gmail.com>
2020-10-31 13:29:13 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StaticSiteGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
