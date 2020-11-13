057023d3d9b7399d5b457f15573a7b69f346efd8
zpdelong <zacherypd@gmail.com>
2020-11-12 21:07:45 -0500

    Added local configuration for building and testing in Emacs

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;;; Project-specific emacs variables
+
+((csharp-mode . ((projectile-project-compilation-cmd . "dotnet run --project src")
+         (projectile-project-test-cmd . "dotnet test ./test"))))
