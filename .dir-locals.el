5f8d028c498facdca07de79074910525c3d7694c
Zachery DeLong <zacherypd@gmail.com>
2020-12-21 08:52:27 -0500

    Fixing compile/test commands in Emacs for new project structure

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ;;;; Project-specific emacs variables
 
-((csharp-mode . ((projectile-project-compilation-cmd . "dotnet run --project src")
-         (projectile-project-test-cmd . "dotnet test ./test"))))
+((csharp-mode . ((projectile-project-compilation-cmd . "dotnet run --project StaticSiteGenerator")
+         (projectile-project-test-cmd . "dotnet test"))))
