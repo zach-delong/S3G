68e2448a02b931e3c7ef022f20a5cc8bd93fa7f3
Zachery DeLong <zacherypd@gmail.com>
2020-12-23 09:40:40 -0500

    Markdown Block Converter uses strategy collection

--- StaticSiteGenerator.UnitTests/TemplateSubstitution/MarkdownBlockConverterTest.cs
+++ StaticSiteGenerator.UnitTests/TemplateSubstitution/MarkdownBlockConverterTest.cs
@@ -6,18 +6,23 @@ using StaticSiteGenerator.TemplateSubstitution;
 using StaticSiteGenerator.TemplateSubstitution.MarkdownHtmlConverters;
 using StaticSiteGenerator.TemplateSubstitution.BlockConverterStrategies;
 using StaticSiteGenerator.Markdown.BlockElement;
+using Test.Markdown.Parser;
+using StaticSiteGenerator.Utilities.StrategyPattern;
 
 namespace Test.TemplateSubstitution
 {
     public class MarkdownBlockConverterTest
     {
+        StrategyCollectionMockFactory mockFactory => new StrategyCollectionMockFactory();
         [Fact]
         public void ConverterCallsCorrectStrategyWhenExists()
         {
             TestConverter testConverter = new TestConverter();
-            var converter = new MarkdownBlockConverter(new List<IBlockHtmlConverterStrategy> {
-                    testConverter
-                        });
+            var dict = new Dictionary<string, IBlockHtmlConverterStrategy> {
+                { nameof(Header), testConverter }
+            };
+            var mock = mockFactory.Get<IBlockHtmlConverterStrategy>(dict);
+            var converter = new MarkdownBlockConverter(mock.Object);
 
             var block = new Header();
 
@@ -29,11 +34,11 @@ namespace Test.TemplateSubstitution
         [Fact]
         public void ConverterThrowsExceptionWhenNoMatchingStrategyExists()
         {
-            var converter = new MarkdownBlockConverter(new List<IBlockHtmlConverterStrategy>());
+            var converter = new MarkdownBlockConverter(mockFactory.Get(new Dictionary<string, IBlockHtmlConverterStrategy>()).Object);
 
             var block = new Header();
 
-            Assert.Throws<Exception>(() => { converter.Convert(block); });
+            Assert.Throws<StrategyNotFoundException>(() => { converter.Convert(block); });
         }
 
         [HtmlConverterFor(nameof(Header))]
--- StaticSiteGenerator/TemplateSubstitution/MarkdownHtmlConverters/MarkdownBlockConverter.cs
+++ StaticSiteGenerator/TemplateSubstitution/MarkdownHtmlConverters/MarkdownBlockConverter.cs
@@ -8,15 +8,16 @@ using StaticSiteGenerator.Markdown.BlockElement;
 
 using TanvirArjel.Extensions.Microsoft.DependencyInjection;
 using System.Linq;
+using StaticSiteGenerator.Utilities.StrategyPattern;
 
 namespace StaticSiteGenerator.TemplateSubstitution.MarkdownHtmlConverters
 {
     [TransientService]
     public class MarkdownBlockConverter: IHtmlConverter<IList<IBlockElement>>, IHtmlConverter<IBlockElement>
     {
-        private readonly IEnumerable<IBlockHtmlConverterStrategy> BlockConverters;
+        private readonly StrategyCollection<IBlockHtmlConverterStrategy> BlockConverters;
 
-        public MarkdownBlockConverter(IEnumerable<IBlockHtmlConverterStrategy> blockConverters)
+        public MarkdownBlockConverter(StrategyCollection<IBlockHtmlConverterStrategy> blockConverters)
         {
             BlockConverters = blockConverters;
         }
@@ -28,31 +29,9 @@ namespace StaticSiteGenerator.TemplateSubstitution.MarkdownHtmlConverters
 
         public string Convert(IBlockElement block)
         {
-            var blockConverter = GetConverterFor(block.GetType());
+            var blockConverter = BlockConverters.GetConverterForType(block.GetType());
 
             return blockConverter.Convert(block);
-        }
-
-        private IHtmlConverter<IBlockElement> GetConverterFor(Type t)
-        {
-            foreach(var converter in BlockConverters)
-            {
-                if(ConverterMatchesAttributeType(converter, t))
-                {
-                    return converter;
-                }
-            }
-
-            throw new Exception($"Could not find an HTML Writer for {t.Name}");
-        }
-
-        private bool ConverterMatchesAttributeType(IHtmlConverter<IBlockElement> converter, Type t)
-        {
-          var converterType = converter.GetType();
-
-          var attr = (HtmlConverterForAttribute) Attribute.GetCustomAttribute(converterType, typeof(HtmlConverterForAttribute));
-
-          return attr?.TypeName == t.Name;
         }
     }
 }
