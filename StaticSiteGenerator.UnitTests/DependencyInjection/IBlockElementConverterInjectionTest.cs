c95e864a3be4ebfb01ceba1bd2e5a29df6016dc1
Zachery DeLong <zacherypd@gmail.com>
2021-01-13 19:58:30 -0500

    Testing BlockElement converter DI

---
+++ StaticSiteGenerator.UnitTests/DependencyInjection/IBlockElementConverterInjectionTest.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using StaticSiteGenerator.Markdown.BlockElementConverter;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using Xunit;
+
+namespace StaticSiteGenerator.UnitTests.DependencyInjection
+{
+    public class IBlockElementConverterInjectionTest
+    {
+        public void foo()
+        {
+            var diContainer = Program.BuildDependencies(new CliOptions());
+
+            var converters = diContainer
+                .GetService<IEnumerable<IBlockElementConverter>>()
+                .ToDictionary(c => c.GetType(), c => c);
+
+            Assert.NotNull(converters[typeof(HeaderConverter)]);
+            Assert.NotNull(converters[typeof(ParagraphConverter)]);
+
+            Assert.Collection(converters,
+                              element => Assert.NotNull(element),
+                              element => Assert.NotNull(element));
+        }
+    }
+}
