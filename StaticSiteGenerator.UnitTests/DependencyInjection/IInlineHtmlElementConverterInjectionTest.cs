a61e0fd2fc101ba8f3a9b06e1176c0fe1a0690dc
Zachery DeLong <zacherypd@gmail.com>
2021-01-18 14:16:19 -0500

    Adding more tests of DI

---
+++ StaticSiteGenerator.UnitTests/DependencyInjection/IInlineHtmlElementConverterInjectionTest.cs
@@ -0,0 +1,25 @@
+using StaticSiteGenerator.TemplateSubstitution.InlineConverterStrategies;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StaticSiteGenerator.UnitTests.DependencyInjection
+{
+    public class IInlineHtmlElementConverterInjectionTest
+    {
+        [Fact]
+        public void DishouldReturnHtmlElementConverterTest()
+        {
+            var diContainer = Program.BuildDependencies(new CliOptions());
+
+            var converters = diContainer
+                .GetService<IEnumerable<IInlineConverterStrategy>>()
+                .ToDictionary(c => c.GetType(), c => c);
+
+            Assert.NotNull(converters[typeof(TextConverter)]);
+
+            Assert.Single(converters);
+        }
+    }
+}
