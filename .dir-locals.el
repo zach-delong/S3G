51700655063d4e6d637325068d5aa31239bc6b38
Zachery DeLong <zacherypd@gmail.com>
2021-02-20 19:26:45 -0500

    Changed test command in emacs
    Testing should generate code coverage. I'm okay with that for now.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ;;;; Project-specific emacs variables
 
 ((csharp-mode . ((projectile-project-compilation-cmd . "dotnet run --project StaticSiteGenerator")
-         (projectile-project-test-cmd . "dotnet test"))))
+         (projectile-project-test-cmd . "dotnet test --collect 'XPlat Code Coverage'"))))
