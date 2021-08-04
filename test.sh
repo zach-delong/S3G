91a44dc6bb0a41396f64e5eb8f1f813572e97501
Zachery DeLong <zacherypd@gmail.com>
2021-08-03 21:35:31 -0400

    Fixing script to run as not root if it can.

--- test.sh
+++ test.sh
@@ -1,3 +1,12 @@
-sudo docker build -t s3g-image -f Dockerfile.test .
+# If the current user is in the docker group, we don't need to use sudo.
+# If they are not, then we must run docker as root =()
+if groups $USER | grep "docker" ; then
+    docker build -t s3g-image -f Dockerfile.test .
 
-sudo docker run --rm --name s3g-container s3g-image
+    docker run --rm --name s3g-container s3g-image
+else
+    sudo docker build -t s3g-image -f Dockerfile.test .
+
+    sudo docker run --rm --name s3g-container s3g-image
+fi
+
