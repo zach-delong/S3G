26ef9aa7f3d873d6a2abf0bf30925d195f978a59
Zachery DeLong <zacherypd@gmail.com>
2021-11-29 14:39:34 -0500

    Checking in file/folder reading
    File reading now works correctly and handles differences in paths gracefully

--- StaticSiteGenerator.UnitTests/Files/FileProcessingStrategies/FileProcessingStrategyTests.cs
+++ StaticSiteGenerator.UnitTests/Files/FileProcessingStrategies/FileProcessingStrategyTests.cs
@@ -10,7 +10,7 @@ using Xunit;
 
 namespace StaticSiteGenerator.UnitTests.Files.FileProcessingStrategies
 {
-    public class FolderProcessingStrategyTests
+    public class FileProcessingStrategyTests
     {
         [Theory]
         [MemberData(nameof(TestCaseData))]
@@ -18,7 +18,7 @@ namespace StaticSiteGenerator.UnitTests.Files.FileProcessingStrategies
             MockFileSystem fs,
             string outputLocation,
             string inputLocation,
-            string[] targetFolderNames,
+            string[] targetFileNames,
             string pathToCheck)
         {
             var options = new CliOptions 
@@ -26,15 +26,14 @@ namespace StaticSiteGenerator.UnitTests.Files.FileProcessingStrategies
                 OutputLocation = outputLocation,
                 PathToMarkdownFiles = inputLocation
             };
+            var sut = new FileProcessingStrategy(fs, options);
 
-            var sut = new FolderProcessingStrategy(fs, options);
-
-            foreach(var folder in targetFolderNames)
+            foreach(var file in targetFileNames)
             {
-                sut.Execute(new Folder(folder));
+                sut.Execute(new StaticSiteGenerator.Files.File(file));
             }
 
-            var success = fs.AllDirectories.Any(d => d == pathToCheck);
+            var success = fs.AllFiles.Any(d => d == pathToCheck);
 
             Assert.True(success);
         }
@@ -44,26 +43,26 @@ namespace StaticSiteGenerator.UnitTests.Files.FileProcessingStrategies
             get
             {
                 var system1 = new MockFileSystem();
-
-                system1.Directory.SetCurrentDirectory(system1.Path.GetSystemRoot());
                 system1.AddDirectory("outputLocation");
-                system1.AddDirectory(system1.Path.Combine("inputLocation", "newFolder"));
+                system1.AddDirectory("inputLocation");
+                system1.AddFile(system1.Path.Combine("inputLocation", "newFile.jpg"), new MockFileData("foo"));
+                system1.Directory.SetCurrentDirectory(system1.Path.GetSystemRoot());
 
                 // Relative paths all the way down
                 yield return new object[]
                 {
                     system1,
-                    "outputLocation",
-                    "inputLocation",
-                    new string[] { "inputLocation/newFolder" },
-                    $"{system1.Path.GetSystemRoot()}{system1.Path.Join("outputLocation", "newFolder")}"
+                    "/outputLocation",
+                    "/inputLocation",
+                    new string[] { "inputLocation/newFile.jpg" },
+                    $"{system1.Path.GetSystemRoot()}{system1.Path.Join("outputLocation", "newFile.jpg")}"
                 };
 
                 var system2 = new MockFileSystem();
 
                 system2.AddDirectory("workspace");
                 system2.AddDirectory(system2.Path.Combine("workspace", "input"));
-                system2.AddDirectory(system2.Path.Combine("workspace", "input", "target"));
+                system2.AddFile(system2.Path.Combine("workspace", "input", "target.jpg"), new MockFileData("foo"));
                 system2.AddDirectory(system2.Path.Combine("workspace", "output"));
                 system2.Directory.SetCurrentDirectory(system2.Path.Combine(system2.Path.GetSystemRoot(), "workspace"));
 
@@ -71,15 +70,16 @@ namespace StaticSiteGenerator.UnitTests.Files.FileProcessingStrategies
                 yield return new object[]
                 {
                     system2,
-                    "output", 
+                    "output",
                     "input",
-                    new string[] { "input/target" },
-                    system2.Path.Combine(system2.Path.GetSystemRoot(), "workspace", "output", "target")
+                    new string[] { "input/target.jpg" },
+                    system2.Path.Combine(system2.Path.GetSystemRoot(), "workspace", "output", "target.jpg")
                 };
 
                 var system3 = new MockFileSystem();
                 system3.AddDirectory("workspace");
                 system3.AddDirectory(system3.Path.Combine("workspace", "input"));
+                system3.AddFile(system3.Path.Combine("workspace", "input", "target.jpg"), new MockFileData("foo"));
                 system3.AddDirectory(system3.Path.Combine("workspace", "output"));
                 system3.Directory.SetCurrentDirectory(system3.Path.Combine(system3.Path.GetSystemRoot(), "workspace"));
 
@@ -89,8 +89,8 @@ namespace StaticSiteGenerator.UnitTests.Files.FileProcessingStrategies
                     system3,
                     "/workspace/output",
                     "/workspace/input",
-                    new string[] { "input/target" },
-                    system3.Path.Combine(system3.Path.GetSystemRoot(), "workspace", "output", "target")
+                    new string[] { "input/target.jpg" },
+                    system3.Path.Combine(system3.Path.GetSystemRoot(), "workspace", "output", "target.jpg")
                 };
             }
         }
--- StaticSiteGenerator/Files/FileProcessingStrategies/FileProcessingStrategy.cs
+++ StaticSiteGenerator/Files/FileProcessingStrategies/FileProcessingStrategy.cs
@@ -3,27 +3,27 @@ using StaticSiteGenerator.Utilities.StrategyPattern;
 
 namespace StaticSiteGenerator.Files.FileProcessingStrategies
 {
-    [FileProcessorForType(nameof(Folder))]
-    public class FolderProcessingStrategy : IStrategy<object, IFileSystemObject>
+    [FileProcessorForType(nameof(File))]
+    public class FileProcessingStrategy : IStrategy<object, IFileSystemObject>
     {
         private readonly IFileSystem system;
         private readonly CliOptions options;
 
-        public FolderProcessingStrategy(IFileSystem system, CliOptions options)
+        public FileProcessingStrategy(IFileSystem system, CliOptions options)
         {
             this.system = system;
             this.options = options;
         }
-
         public object Execute(IFileSystemObject input)
         {
             var rootPathToInputFile = system.Path.GetFullPath(input.Name);
             var rootPathToInputRoot = system.Path.GetFullPath(options.PathToMarkdownFiles);
-
+            
             var inputPathRelativeToInputRoot = system.Path.GetRelativePath(rootPathToInputRoot, rootPathToInputFile);
-            var path = system.Path.Join(options.OutputLocation, inputPathRelativeToInputRoot);
-            system.Directory.CreateDirectory(path);
+            string destinationFilePath = system.Path.Combine(options.OutputLocation, inputPathRelativeToInputRoot);
 
+            system.File.Copy(input.Name, destinationFilePath);
+            
             return null;
         }
     }
