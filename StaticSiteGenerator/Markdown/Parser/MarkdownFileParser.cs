d6726ec761ce49f4e9d0e0cfa3303e1e1077d80d
Zachery DeLong <zacherypd@gmail.com>
2021-12-11 14:06:52 -0500

    Single file name-space

--- StaticSiteGenerator/Markdown/Parser/MarkdownFileParser.cs
+++ StaticSiteGenerator/Markdown/Parser/MarkdownFileParser.cs
@@ -8,53 +8,52 @@ using Markdig.Syntax;
 using System.Linq;
 using StaticSiteGenerator.Files;
 
-namespace StaticSiteGenerator.Markdown.Parser
+namespace StaticSiteGenerator.Markdown.Parser;
+
+public class MarkdownFileParser : IMarkdownFileParser
 {
-    public class MarkdownFileParser : IMarkdownFileParser
+    private readonly FileReader fileParser;
+    private readonly IStrategyExecutor<IBlockElement, IBlock> markdownParser;
+    private readonly ILogger<MarkdownFileParser> logger;
+
+    public MarkdownFileParser(
+        FileReader fileParser,
+        IStrategyExecutor<IBlockElement, IBlock> markdownParser,
+        ILogger<MarkdownFileParser> logger
+    )
     {
-        private readonly FileReader fileParser;
-        private readonly IStrategyExecutor<IBlockElement, IBlock> markdownParser;
-        private readonly ILogger<MarkdownFileParser> logger;
+        this.fileParser = fileParser;
+        this.markdownParser = markdownParser;
+        this.logger = logger;
+    }
 
-        public MarkdownFileParser(
-            FileReader fileParser,
-            IStrategyExecutor<IBlockElement, IBlock> markdownParser,
-            ILogger<MarkdownFileParser> logger
-        )
+    public IMarkdownFile ReadFile(string filePath)
+    {
+        logger.LogInformation("Starting to convert string contents to Markdown");
+        logger.LogTrace($"Starting to convert file {filePath}");
+
+        logger.LogTrace($"Reading file: {filePath}");
+        var fileContents = fileParser.ReadFile(filePath);
+        logger.LogTrace($"Read file contents: {fileContents?.Substring(0, ((fileContents.Length > 50) ? 50 : fileContents.Length)) ?? String.Empty}");
+        var parsedContents = ParseMarkdownString(fileContents);
+
+        IMarkdownFile file = new MarkdownFile
         {
-            this.fileParser = fileParser;
-            this.markdownParser = markdownParser;
-            this.logger = logger;
-        }
+            Elements = parsedContents,
+            Name = Path.GetFileNameWithoutExtension(filePath)
+        };
 
-        public IMarkdownFile ReadFile(string filePath)
-        {
-            logger.LogInformation("Starting to convert string contents to Markdown");
-            logger.LogTrace($"Starting to convert file {filePath}");
+        logger.LogTrace($"Converted file: {filePath}");
 
-            logger.LogTrace($"Reading file: {filePath}");
-            var fileContents = fileParser.ReadFile(filePath);
-            logger.LogTrace($"Read file contents: {fileContents?.Substring(0, ((fileContents.Length > 50) ? 50 : fileContents.Length)) ?? String.Empty}");
-            var parsedContents = ParseMarkdownString(fileContents);
+        return file;
+    }
 
-            IMarkdownFile file = new MarkdownFile
-            {
-                Elements = parsedContents,
-                Name = Path.GetFileNameWithoutExtension(filePath)
-            };
+    private IList<IBlockElement> ParseMarkdownString(string markdownFileContents)
+    {
+        var document = Markdig.Markdown.Parse(markdownFileContents);
 
-            logger.LogTrace($"Converted file: {filePath}");
+        var interalMarkdownTypedFile = markdownParser.Process(document);
 
-            return file;
-        }
-
-        private IList<IBlockElement> ParseMarkdownString(string markdownFileContents)
-        {
-            var document = Markdig.Markdown.Parse(markdownFileContents);
-
-            var interalMarkdownTypedFile = markdownParser.Process(document);
-
-            return interalMarkdownTypedFile.ToList();
-        }
+        return interalMarkdownTypedFile.ToList();
     }
 }
