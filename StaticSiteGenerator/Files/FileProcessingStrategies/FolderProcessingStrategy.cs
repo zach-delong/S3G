13362ae09eb39853e7a69950d8141f9e58674b61
Zachery DeLong <zacherypd@gmail.com>
2021-12-11 13:54:51 -0500

    Converted file to single file namespace

--- StaticSiteGenerator/Files/FileProcessingStrategies/FolderProcessingStrategy.cs
+++ StaticSiteGenerator/Files/FileProcessingStrategies/FolderProcessingStrategy.cs
@@ -1,30 +1,29 @@
 using System.IO.Abstractions;
 using StaticSiteGenerator.Utilities.StrategyPattern;
 
-namespace StaticSiteGenerator.Files.FileProcessingStrategies
+namespace StaticSiteGenerator.Files.FileProcessingStrategies;
+
+[FileProcessorForType(nameof(FolderFileSystemObject))]
+public class FolderProcessingStrategy : IStrategy<object, IFileSystemObject>
 {
-    [FileProcessorForType(nameof(FolderFileSystemObject))]
-    public class FolderProcessingStrategy : IStrategy<object, IFileSystemObject>
+    private readonly IFileSystem system;
+    private readonly CliOptions options;
+
+    public FolderProcessingStrategy(IFileSystem system, CliOptions options)
     {
-        private readonly IFileSystem system;
-        private readonly CliOptions options;
+        this.system = system;
+        this.options = options;
+    }
 
-        public FolderProcessingStrategy(IFileSystem system, CliOptions options)
-        {
-            this.system = system;
-            this.options = options;
-        }
+    public object Execute(IFileSystemObject input)
+    {
+        var rootPathToInputFile = system.Path.GetFullPath(input.FullPath);
+        var rootPathToInputRoot = system.Path.GetFullPath(options.PathToMarkdownFiles);
 
-        public object Execute(IFileSystemObject input)
-        {
-            var rootPathToInputFile = system.Path.GetFullPath(input.FullPath);
-            var rootPathToInputRoot = system.Path.GetFullPath(options.PathToMarkdownFiles);
+        var inputPathRelativeToInputRoot = system.Path.GetRelativePath(rootPathToInputRoot, rootPathToInputFile);
+        var path = system.Path.Join(options.OutputLocation, inputPathRelativeToInputRoot);
+        system.Directory.CreateDirectory(path);
 
-            var inputPathRelativeToInputRoot = system.Path.GetRelativePath(rootPathToInputRoot, rootPathToInputFile);
-            var path = system.Path.Join(options.OutputLocation, inputPathRelativeToInputRoot);
-            system.Directory.CreateDirectory(path);
-
-            return null;
-        }
+        return null;
     }
 }
