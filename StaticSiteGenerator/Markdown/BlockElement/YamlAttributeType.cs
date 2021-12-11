3720086fedda8aa16d6c723dda7f4a7182a5b9f6
Zachery DeLong <zacherypd@gmail.com>
2021-12-11 14:02:39 -0500

    Single file name spaces

--- StaticSiteGenerator/Markdown/BlockElement/YamlAttributeType.cs
+++ StaticSiteGenerator/Markdown/BlockElement/YamlAttributeType.cs
@@ -3,45 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
-namespace StaticSiteGenerator.Markdown.BlockElement
+namespace StaticSiteGenerator.Markdown.BlockElement;
+
+public class YamlAttributeType
 {
-    public class YamlAttributeType
+    public static YamlAttributeType PublishDate = new YamlAttributeType("publish_date");
+
+    public string Name { get; set; }
+    private static IList<YamlAttributeType> values;
+    public static IList<YamlAttributeType> Values
     {
-        public static YamlAttributeType PublishDate = new YamlAttributeType("publish_date");
-
-        public string Name { get; set; }
-        private static IList<YamlAttributeType> values;
-        public static IList<YamlAttributeType> Values
+        get
         {
-            get
-            {
-                values ??= List();
-                return values;
-            }
+            values ??= List();
+            return values;
         }
+    }
 
-        private YamlAttributeType(string name)
-        {
-            Name = name;
-        }
+    private YamlAttributeType(string name)
+    {
+        Name = name;
+    }
 
-        public static bool IsValidValue(string input)
-        {
-            return Values.Select(v => v.Name).Contains(input);
-        }
+    public static bool IsValidValue(string input)
+    {
+        return Values.Select(v => v.Name).Contains(input);
+    }
 
-        public static YamlAttributeType GetFromStringOrDefault(string input)
-        {
-            return Values.SingleOrDefault(v => v.Name == input);
-        }
+    public static YamlAttributeType GetFromStringOrDefault(string input)
+    {
+        return Values.SingleOrDefault(v => v.Name == input);
+    }
 
-        private static IList<YamlAttributeType> List()
-        {
-            return typeof(YamlAttributeType).GetFields(BindingFlags.Public | BindingFlags.Static)
-                                            .Where(p => p.FieldType == typeof(YamlAttributeType))
-                                            .Select(p => (YamlAttributeType)p.GetValue(null))
-                                            .OrderBy(p => p.Name)
-                                            .ToList();
-        }
+    private static IList<YamlAttributeType> List()
+    {
+        return typeof(YamlAttributeType).GetFields(BindingFlags.Public | BindingFlags.Static)
+                                        .Where(p => p.FieldType == typeof(YamlAttributeType))
+                                        .Select(p => (YamlAttributeType)p.GetValue(null))
+                                        .OrderBy(p => p.Name)
+                                        .ToList();
     }
 }
