3f719af3369ee0988fdea6d9c662bbe949b8e8ea
Zachery DeLong <zacherypd@gmail.com>
2021-12-11 14:07:41 -0500

    Single file name-space

--- StaticSiteGenerator/Markdown/ServicesConfiguration.cs
+++ StaticSiteGenerator/Markdown/ServicesConfiguration.cs
@@ -9,21 +9,20 @@ using StaticSiteGenerator.Markdown.InlineElement;
 using StaticSiteGenerator.Markdown.BlockElement;
 using Markdig.Syntax;
 
-namespace StaticSiteGenerator.Markdown
+namespace StaticSiteGenerator.Markdown;
+
+public static class ServicesConfiguration
 {
-    public static class ServicesConfiguration
+    public static void AddMarkdownConverters(this IServiceCollection services)
     {
-        public static void AddMarkdownConverters(this IServiceCollection services)
-        {
-            services.AddTransient<IStrategy<IInlineElement, IInline>, TextElementConverter>();
+        services.AddTransient<IStrategy<IInlineElement, IInline>, TextElementConverter>();
 
-            services.AddTransient<IStrategy<IBlockElement, IBlock>, HeaderConverter>();
-            services.AddTransient<IStrategy<IBlockElement, IBlock>, ParagraphConverter>();
-        }
+        services.AddTransient<IStrategy<IBlockElement, IBlock>, HeaderConverter>();
+        services.AddTransient<IStrategy<IBlockElement, IBlock>, ParagraphConverter>();
+    }
 
-        public static void AddMarkdownParsers(this IServiceCollection services)
-        {
-            services.AddTransient<IMarkdownFileParser, MarkdownFileParser>();
-        }
+    public static void AddMarkdownParsers(this IServiceCollection services)
+    {
+        services.AddTransient<IMarkdownFileParser, MarkdownFileParser>();
     }
 }
