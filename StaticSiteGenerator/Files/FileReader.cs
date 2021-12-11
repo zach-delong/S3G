51a23589fe53947651d5de1469d2771419c0f53d
Zachery DeLong <zacherypd@gmail.com>
2021-12-11 13:57:11 -0500

    Converted file to single file namespace

--- StaticSiteGenerator/Files/FileReader.cs
+++ StaticSiteGenerator/Files/FileReader.cs
@@ -2,26 +2,25 @@ using System.IO;
 using System.IO.Abstractions;
 using StaticSiteGenerator.Files.FileException;
 
-namespace StaticSiteGenerator.Files
-{
-    public class FileReader
-    {
-        private readonly IFileSystem fileSystem;
+namespace StaticSiteGenerator.Files;
 
-        public FileReader(IFileSystem fileSystem)
+public class FileReader
+{
+    private readonly IFileSystem fileSystem;
+
+    public FileReader(IFileSystem fileSystem)
+    {
+        this.fileSystem = fileSystem;
+    }
+    public virtual string ReadFile(string filePath)
+    {
+        try
         {
-            this.fileSystem = fileSystem;
+            return fileSystem.File.ReadAllText(filePath);
         }
-        public virtual string ReadFile(string filePath)
+        catch (FileNotFoundException ex)
         {
-            try
-            {
-                return fileSystem.File.ReadAllText(filePath);
-            }
-            catch (FileNotFoundException ex)
-            {
-                throw new FileManipulationException($"Error, the file {filePath} was not found when attempting to read it", ex);
-            }
+            throw new FileManipulationException($"Error, the file {filePath} was not found when attempting to read it", ex);
         }
     }
 }
