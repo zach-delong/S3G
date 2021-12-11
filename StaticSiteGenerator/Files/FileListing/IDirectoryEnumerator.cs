858ff00a717a9be235359ae30947455cc2a7d2d8
Zachery DeLong <zacherypd@gmail.com>
2021-12-11 13:53:28 -0500

    Converted file to single file namespace

--- StaticSiteGenerator/Files/FileListing/IDirectoryEnumerator.cs
+++ StaticSiteGenerator/Files/FileListing/IDirectoryEnumerator.cs
@@ -1,22 +1,21 @@
 using System.Collections.Generic;
 
-namespace StaticSiteGenerator.Files.FileListing
+namespace StaticSiteGenerator.Files.FileListing;
+
+/// <summary>
+/// Given a path to a directory, give a list of items in that directory
+/// </summary
+public interface IDirectoryEnumerator
 {
     /// <summary>
-    /// Given a path to a directory, give a list of items in that directory
+    /// Given a path and a pattern, get all files (not directories) in the
+    /// directory that match the given pattern.
     /// </summary
-    public interface IDirectoryEnumerator
-    {
-        /// <summary>
-        /// Given a path and a pattern, get all files (not directories) in the
-        /// directory that match the given pattern.
-        /// </summary
-        public IEnumerable<string> GetFiles(string path, string pattern);
+    public IEnumerable<string> GetFiles(string path, string pattern);
 
-        /// <summary>
-        /// Given a path, returns all folders and files in the path, "recursively"
-        /// Will always give you back folders before the files inside them
-        /// </summary
-        public IEnumerable<IFileSystemObject> ListAllContents(string path);
-    }
+    /// <summary>
+    /// Given a path, returns all folders and files in the path, "recursively"
+    /// Will always give you back folders before the files inside them
+    /// </summary
+    public IEnumerable<IFileSystemObject> ListAllContents(string path);
 }
