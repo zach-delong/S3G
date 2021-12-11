b3f570a73f55e14c4dc307987bab6899e29c5daa
Zachery DeLong <zacherypd@gmail.com>
2021-12-11 14:02:14 -0500

    Single file name spaces

--- StaticSiteGenerator/Markdown/BlockElement/Paragraph.cs
+++ StaticSiteGenerator/Markdown/BlockElement/Paragraph.cs
@@ -1,14 +1,13 @@
 using System.Collections.Generic;
 using StaticSiteGenerator.Markdown.InlineElement;
 
-namespace StaticSiteGenerator.Markdown.BlockElement
+namespace StaticSiteGenerator.Markdown.BlockElement;
+
+public class Paragraph : IBlockElement
 {
-    public class Paragraph: IBlockElement
-    {
-        public IList<IInlineElement> Inlines { get; set; }
+    public IList<IInlineElement> Inlines { get; set; }
 
-        public string Text { get; set; }
+    public string Text { get; set; }
 
-        public string Content => Text;
-    }
+    public string Content => Text;
 }
