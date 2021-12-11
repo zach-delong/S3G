26b6651132b102295e2d4850a4fb94d439ce20c7
Zachery DeLong <zacherypd@gmail.com>
2021-12-11 14:07:31 -0500

    Single file name-space

--- StaticSiteGenerator/Markdown/MarkdownFile.cs
+++ StaticSiteGenerator/Markdown/MarkdownFile.cs
@@ -2,13 +2,12 @@ using System;
 using System.Collections.Generic;
 using StaticSiteGenerator.Markdown.BlockElement;
 
-namespace StaticSiteGenerator.Markdown
+namespace StaticSiteGenerator.Markdown;
+
+public class MarkdownFile : IMarkdownFile
 {
-    public class MarkdownFile: IMarkdownFile
-    {
-        public IList<IBlockElement> Elements { get; set; }
-        public string Name { get; set; }
-        public string FileExtension => ".md";
-        public DateTime PublishedDate { get; set; }
-    }
+    public IList<IBlockElement> Elements { get; set; }
+    public string Name { get; set; }
+    public string FileExtension => ".md";
+    public DateTime PublishedDate { get; set; }
 }
