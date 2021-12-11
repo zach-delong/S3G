6a3608c5859ecdb0d4867e6205f562b5aadeb78d
Zachery DeLong <zacherypd@gmail.com>
2021-12-11 13:59:31 -0500

    Converted to single file namespaces

--- StaticSiteGenerator/HtmlWriting/FileSystemHtmlWriter.cs
+++ StaticSiteGenerator/HtmlWriting/FileSystemHtmlWriter.cs
@@ -1,23 +1,19 @@
-using System;
-using System.Collections.Generic;
 using StaticSiteGenerator.Files.FileWriting;
-using StaticSiteGenerator.MarkdownHtmlConversion;
 
-namespace StaticSiteGenerator.HtmlWriting
+namespace StaticSiteGenerator.HtmlWriting;
+
+public class FileSystemHtmlWriter : IHtmlFileWriter
 {
-    public class FileSystemHtmlWriter: IHtmlFileWriter
+    public FileSystemHtmlWriter(IFileWriter writer)
     {
-        public FileSystemHtmlWriter(IFileWriter writer)
-        {
-            Writer = writer;
-        }
+        Writer = writer;
+    }
 
-        private const string HtmlFileExtension = ".html";
-        private readonly IFileWriter Writer;
+    private const string HtmlFileExtension = ".html";
+    private readonly IFileWriter Writer;
 
-        public void Write(string filePath, string htmlString)
-        {
-            Writer.WriteFile(filePath, htmlString);
-        }
+    public void Write(string filePath, string htmlString)
+    {
+        Writer.WriteFile(filePath, htmlString);
     }
 }
