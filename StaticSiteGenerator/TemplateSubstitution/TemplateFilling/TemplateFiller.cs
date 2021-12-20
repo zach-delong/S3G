6ca272a7d7b9cd34ef7b930797ac6ade171b1d4b
Zachery DeLong <zacherypd@gmail.com>
2021-12-19 20:12:06 -0500

    Adding TemplateFiller tests

--- StaticSiteGenerator/TemplateSubstitution/TemplateFilling/TemplateFiller.cs
+++ StaticSiteGenerator/TemplateSubstitution/TemplateFilling/TemplateFiller.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
 using StaticSiteGenerator.TemplateSubstitution.TemplateTags;
+using StaticSiteGenerator.Utilities.Extensions;
 
 namespace StaticSiteGenerator.TemplateSubstitution.TemplateFilling;
 
@@ -7,5 +10,15 @@ public class TemplateFiller : ITemplateFiller
     public string Fill(TemplateTag tag, string content)
     {
         return tag.Template.Replace("{{}}", content);
+    }
+
+    public string Fill(TemplateTag tag, IDictionary<string, string> content)
+    {
+        var result = tag.Template;
+
+        var replacements = content
+            .ToDictionary(kvp => $"{{{{{kvp.Key}}}}}", kvp => kvp.Value);
+
+        return result.Replace(replacements);
     }
 }
