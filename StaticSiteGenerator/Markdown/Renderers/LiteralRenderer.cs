fc1807317b5ef6528bae536781d7a6e1aec96087
Zachery DeLong <zacherypd@gmail.com>
2022-01-23 14:37:56 -0500

    Added abstract class to make future renderers easier to write

--- StaticSiteGenerator/Markdown/Renderers/LiteralRenderer.cs
+++ StaticSiteGenerator/Markdown/Renderers/LiteralRenderer.cs
@@ -1,15 +1,10 @@
-using System;
 using Markdig.Renderers;
-using Markdig.Renderers.Html;
-using Markdig.Renderers.Html.Inlines;
 using Markdig.Syntax.Inlines;
 
 namespace StaticSiteGenerator.Markdown.Renderers;
 
-public class LiteralRenderer : HtmlObjectRenderer<LiteralInline>, ICustomRenderer
+public class LiteralRenderer : CustomRendererBase<LiteralInline>
 {
-    public Type Replaces => typeof(LiteralInlineRenderer);
-
     protected override void Write(HtmlRenderer renderer, LiteralInline obj)
     {
         if(obj == null){
