63ae2806b76b1612140637cf549b156160543e0f
Zachery DeLong <zacherypd@gmail.com>
2022-02-20 14:56:22 -0500

    Removed unneeded error case
    The default help text is sufficient for now, so this is not needed

--- StaticSiteGenerator/Program.cs
+++ StaticSiteGenerator/Program.cs
@@ -12,8 +12,7 @@ public class Program
         try
         {
             CommandLine.Parser.Default.ParseArguments<CliOptions>(args)
-                .WithParsed(RunProgram)
-                .WithNotParsed(OptionsErrors);
+                .WithParsed(RunProgram);
         }
         catch (Exception ex)
         {
@@ -37,12 +36,6 @@ public class Program
         service.AddSingleton(options);
 
         return service.BuildServiceProvider();
-    }
-
-    static void OptionsErrors(IEnumerable<Error> errors)
-    {
-        Console.WriteLine(errors);
-        throw new Exception("An error occurred while gathering options");
     }
 
 }
