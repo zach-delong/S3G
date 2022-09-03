46c359bd468107ba1d7a66f702e975126d2c8cc6
Zachery DeLong <zacherypd@gmail.com>
2022-09-03 14:05:20 -0400

    Refactored header 2 integration test

--- StaticSiteGenerator.IntegrationTests/Tests/Header1IntegrationTests.cs
+++ StaticSiteGenerator.IntegrationTests/Tests/Header1IntegrationTests.cs
@@ -8,24 +8,6 @@ namespace StaticSiteGenerator.IntegrationTests.Tests;
 public class HeaderIntegrationTests : IntegrationTestBase
 {
     [Fact]
-    public void Header2ShouldParseCorrectly()
-    {
-        FileSystemCache.AddFile("templates/template/tag_templates/h2.html", new MockFileData("<h2 class='testing'>{{}}</h2>\n"));
-        FileSystemCache.AddFile("templates/template/tag_templates/p.html", new MockFileData("<p>{{}}</p>"));
-        FileSystemCache.AddFile("templates/template/site_template.html", new MockFileData("<html>{{}}</html>"));
-        FileSystemCache.AddDirectory("output");
-        FileSystemCache.AddFile("input/file1.md", new MockFileData("## This is some text!"));
-
-        ServiceProvider.GetService<Generator>().Start();
-
-        const string expectedFileContent = "<html><h2 class='testing'>This is some text!</h2>\n</html>";
-        const string expectedFileName = "/output/file1.html";
-
-        Assert.True(this.FileExists(expectedFileName));
-        Assert.Equal(expectedFileContent, this.ReadFileContents(expectedFileName));
-    }
-
-    [Fact]
     public void Header3ShouldParseCorrectly()
     {
         FileSystemCache.AddFile("templates/template/tag_templates/h3.html", new MockFileData("<h3 class='testing'>{{}}</h3>\n"));
---
+++ StaticSiteGenerator.IntegrationTests/Tests/Headers/Header2IntegrationTest.cs
@@ -0,0 +1,30 @@
+using System.IO.Abstractions.TestingHelpers;
+using StaticSiteGenerator.IntegrationTests.Utilities;
+
+namespace StaticSiteGenerator.IntegrationTests.Tests.Headers;
+
+public class Header2IntegrationTest : SimpleIntegrationTest
+{
+    protected override void Arrange() {
+        var data = new[] {
+	    ("templates/template/tag_templates/h2.html", new MockFileData("<h2 class='testing'>{{}}</h2>\n")),
+	    ("templates/template/tag_templates/p.html", new MockFileData("<p>{{}}</p>")),
+	    ("templates/template/site_template.html", new MockFileData("<html>{{}}</html>")),
+	    ("output", null),
+	    ("input/file1.md", new MockFileData("## This is some text!"))
+	};
+
+        this.CreateFileSystemWith(data);
+    }
+
+    protected override void Act() {
+        this.GenerateHtml();
+    }
+
+    protected override void Assert() {
+        const string expectedFileContent = "<html><h2 class='testing'>This is some text!</h2>\n</html>";
+        const string expectedFileName = "/output/file1.html";
+
+        this.AssertFilesExistWithContents(new[] { (expectedFileName, expectedFileContent) });
+    }
+}
