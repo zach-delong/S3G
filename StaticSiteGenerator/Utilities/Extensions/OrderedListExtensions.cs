40aa666fc039db944af21cdd33a67dd4af241d25
Zachery DeLong <zacherypd@gmail.com>
2023-04-22 14:39:37 -0400

    Fixed bug with renderer replacement
    The renderer replace extension method I added wasn't actually working
    properly, so it was replacing renderers that it shouldn't. I didn't
    notice that anything was wrong until I happened to try to use one of
    the renderers that should have been there, but weren't anymore.
    
    The problem is that the generic types weren't being compared
    properly. The code was only looking at the generic type, not the
    arguments, so Renderer<Inline> was matching for
    Renderer<CodeBlock>. This shouldn't happen anymore!

--- StaticSiteGenerator/Utilities/Extensions/OrderedListExtensions.cs
+++ StaticSiteGenerator/Utilities/Extensions/OrderedListExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Markdig.Helpers;
 using Markdig.Renderers;
 
@@ -13,7 +14,10 @@ public static class OrderedListExtensions
         var found = false;
         for (var i = 0; i < t.Count && !found; i++)
         {
-            if (t[i].GetType().BaseType.Name == toReplace.Name)
+            var baseType = t[i].GetType().BaseType;
+            if (baseType.Name == toReplace.Name
+		&& baseType.GenericTypeArguments.Count() == 1
+		&& baseType.GenericTypeArguments.First().Name == toReplace.GenericTypeArguments.First().Name)
             {
                 t.RemoveAt(i);
                 t.Insert(i, replaceWith);
