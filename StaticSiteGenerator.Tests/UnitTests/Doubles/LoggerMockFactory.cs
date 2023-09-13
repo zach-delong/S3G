9ce434aa4eaa1bbaa5980d176d1b658999fff5c8
Zachery DeLong <zacherypd@gmail.com>
2023-09-12 21:11:29 -0400

    This isn't even referenced, so I removed it.

--- StaticSiteGenerator.Tests/UnitTests/Doubles/LoggerMockFactory.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Extensions.Logging;
-using Moq;
-
-namespace StaticSiteGenerator.Tests.UnitTests.Doubles;
-
-public class LoggerMockFactory
-{
-    public Mock<ILogger<T>> Get<T>() where T : class => new Mock<ILogger<T>>();
-}
