a9c62afdcc0f3d9819a2dec80c11f11e6ec8e264
Zachery DeLong <zacherypd@gmail.com>
2023-09-25 21:17:46 -0400

    Added some comments explaining

--- StaticSiteGenerator.Tests/UnitTests/AutoFixture/MockingTestBase.cs
+++ StaticSiteGenerator.Tests/UnitTests/AutoFixture/MockingTestBase.cs
@@ -3,6 +3,10 @@ using AutoFixture.AutoNSubstitute;
 
 namespace StaticSiteGenerator.Tests.AutoFixture;
 
+///<summary>
+/// This is a base class that sets up an AutoFixture that can be used to
+/// construct systems using NSubstitute mocks as dependencies.
+///</summary>
 public class MockingTestBase
 {
     private IFixture fixture = new Fixture()
